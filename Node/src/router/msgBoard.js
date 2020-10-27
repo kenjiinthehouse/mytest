ea9de15359b65c88574fc01b6572ba06c8035c4e
kenjiinthehouse <69293355+kenjiinthehouse@users.noreply.github.com>
2020-10-28 02:35:25 +0800

    暫存

--- Node/src/router/msgBoard.js
+++ Node/src/router/msgBoard.js
@@ -1,18 +1,22 @@
+const e = require('express');
 const express = require('express');
 const moment = require('moment-timezone');
 const db = require('./../db_connect');
 const router = express.Router();
 
-async function getMsgList(req) {
-  const output = {
-    page: 0,
-    perPage: 10,
-    totalRows: 0,
-    totalPage: 0,
-    rows: [],
-    replyRows:[],
-  };
 
+const output = {
+  page: 0,
+  perPage: 10,
+  totalRows: 0,
+  totalPage: 0,
+  rows: [],
+  replyRows: [],
+};
+
+
+async function getMsgList(req) {
+  
   const [[{ totalRows }]] = await db.query(
     'SELECT COUNT(1) totalRows FROM msgboard'
   );
@@ -42,22 +46,24 @@ async function getMsgList(req) {
     });
 
      result3.forEach((element) => {
-       element.postTime2 = moment(element.postTime).format('YYYY-MM-DD');
+      element.postTime2 = moment(element.postTime).format('YYYY-MM-DD');
      });
+
+
 
     output.rows = result2;
     output.replyRows = result3;
+    
   }
   if (output.rows.length !== 0) {
+    // console.log(output.replyRows[0].sid);
     return output;
+    
   } else {
    return '目前沒有留言';
   }
 }
 
-router.get('/api', async (req, res) => {
-  res.json(await getMsgList(req));
-});
 
 router.get('/', async (req, res) => {
   const output = await getMsgList(req);
@@ -66,6 +72,43 @@ router.get('/', async (req, res) => {
   } else {
     res.render('msgBoard.ejs', output);
   }
+});
+
+router.get('/api', async (req, res) => {
+  res.json(await getMsgList(req));
+});
+
+// //當 url 是 /post/:id 時, 取得某一筆資料
+// app.get('/post/:id', function(req, res, next){
+//   //取得 post.json 資料夾
+//   res.locals.posts.forEach(function(post){
+//     //從 url 取得 id 參數與 posts.json 裡的 id
+//     if (req.params.id === post.id){
+//       //顯示參數為  url 中 id 的 post.id, 那麼顯示部分資料
+//       res.render('post.ejs', { post: post });
+//     }
+//   })
+// });
+
+// router.delete('/del/:sid', async (req, res) => {
+//   const sql = 'DELETE FROM `address_book` WHERE sid=?';
+//   const [results] = await db.query(sql, [req.params.sid]);
+
+//   res.json(results);
+// });
+router.get('/api/relpy', async (req, res) => {
+  const replyCmtSql = `SELECT * FROM msgboard  WHERE parnentId != 0`;
+  const [replyCmt] = await db.query(replyCmtSql);
+  replyCmt.forEach((sid)=>{
+    // output.sid === replyCmt.parnentId;
+    if (output.sid === replyCmt.parnentId) return;
+  }) 
+
+   res.json(replyCmt);
+
+
+
+  // res.render('msgBoard.ejs', outputRelpy);
 });
 
 module.exports = router;
