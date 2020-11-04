f2003b64f5dacc9a315e1e9b962fb983184cbffd
kenjiinthehouse <69293355+kenjiinthehouse@users.noreply.github.com>
2020-11-05 04:35:52 +0800

    navbar 初版

--- reactproj/src/App.js
+++ reactproj/src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
+// 使用antd布局
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+import './styles/kenji.scss'
 // 基本元件
-// import logo from './logo.svg';
 import MyNavbar from './components/MyNavbar';
-import MyFooter from './components/MyFooter';
-import MainContent from './components/MainContent';
+
+
 // 使用 react router 管理
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 // 測試用元件
 // import Counter2 from './pages/Counter2';
 import MsgBoard from './components/MsgBoard';
-import IndexCarousel from './components/IndexCarousel';
-import RecipeReviewCard from './components/RecipeReviewCard';
 
+
+const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
     <Router>
-      <>
-        <MyNavbar />
-        <MainContent>
+      <Layout>
+       <MyNavbar/>
+        <Content>
           <Switch>
             {/* 注意：要加上網址參數 */}
             <Route exact path="/">
               <MsgBoard />
             </Route>
           </Switch>
-        </MainContent>
-        <MyFooter />
-      </>
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
     </Router>
   );
 }
