3062b73872554695c2d2955b84faf84aef18668a
kenjiinthehouse <69293355+kenjiinthehouse@users.noreply.github.com>
2020-11-07 19:59:42 +0800

    update

--- reactproj/src/components/IndexCarousel.js
+++ reactproj/src/components/IndexCarousel.js
@@ -2,33 +2,37 @@ import React from 'react';
 import Carousel from 'react-material-ui-carousel';
 import { Paper, Button } from '@material-ui/core';
 import IndexBailingguoNews from '../img/IndexBailingguoNews.jpg';
+import IndexC2 from '../img/IndexC2.jpg';
+import IndexC3 from '../img/IndexC3.jpg';
+import IndexC4 from '../img/IndexC4.jpg';
+import IndexC5 from '../img/IndexC5.jpg';
 
 function IndexCarousel(props) {
   let items = [
     {
       name: '國際狗語日報X百靈果News',
-      description: '“重新定義你對的自由的想像華語最自由的PODCAST頻道”',
+      description: `“重新定義你對的自由的想像華語最自由的PODCAST頻道”`,
       img: IndexBailingguoNews,
     },
     {
       name: 'Random Name #2',
       description: 'Hello World!',
-      img: IndexBailingguoNews,
+      img: IndexC2,
     },
     {
       name: 'Random Name #3',
       description: 'Hello World!',
-      img: IndexBailingguoNews,
+      img: IndexC3,
     },
     {
       name: 'Random Name #4',
       description: 'Hello World!',
-      img: IndexBailingguoNews,
+      img: IndexC4,
     },
     {
       name: 'Random Name #5',
       description: 'Hello World!',
-      img: IndexBailingguoNews,
+      img: IndexC5,
     },
   ];
 
@@ -49,7 +53,7 @@ function IndexCarousel(props) {
 function Item(props) {
   return (
     <Paper>
-      <h2>{props.item.name}</h2>
+      {/* <h2>{props.item.name}</h2> */}
       <p>{props.item.description}</p>
       <img src={props.item.img} />
       <Button className="CheckButton">Check it out!</Button>
